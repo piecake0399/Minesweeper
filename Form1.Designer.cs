76e707e570d7535a6733463e0e4d7ae5d04c8443
CryoPie <47889641+piecake0399@users.noreply.github.com>
2024-12-09 10:26:44 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Minesweep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_restart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtFlag = new System.Windows.Forms.TextBox();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.explode = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.explode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_restart
+            // 
+            this.btn_restart.Location = new System.Drawing.Point(226, 51);
+            this.btn_restart.Name = "btn_restart";
+            this.btn_restart.Size = new System.Drawing.Size(94, 34);
+            this.btn_restart.TabIndex = 0;
+            this.btn_restart.Text = "Restart";
+            this.btn_restart.UseVisualStyleBackColor = true;
+            this.btn_restart.Click += new System.EventHandler(this.btn_restart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 91);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(545, 506);
+            this.panel1.TabIndex = 1;
+            // 
+            // txtFlag
+            // 
+            this.txtFlag.Location = new System.Drawing.Point(102, 57);
+            this.txtFlag.Name = "txtFlag";
+            this.txtFlag.ReadOnly = true;
+            this.txtFlag.Size = new System.Drawing.Size(85, 22);
+            this.txtFlag.TabIndex = 2;
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(361, 57);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.ReadOnly = true;
+            this.txtScore.Size = new System.Drawing.Size(85, 22);
+            this.txtScore.TabIndex = 3;
+            // 
+            // explode
+            // 
+            this.explode.BackColor = System.Drawing.Color.Transparent;
+            this.explode.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.explode.Cursor = System.Windows.Forms.Cursors.Default;
+            this.explode.Image = global::Minesweep.Properties.Resources.explosion;
+            this.explode.Location = new System.Drawing.Point(20, -6);
+            this.explode.Name = "explode";
+            this.explode.Size = new System.Drawing.Size(76, 76);
+            this.explode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.explode.TabIndex = 0;
+            this.explode.TabStop = false;
+            this.explode.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 601);
+            this.Controls.Add(this.explode);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtFlag);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_restart);
+            this.Name = "Form1";
+            this.Text = "MineSweeper";
+            ((System.ComponentModel.ISupportInitialize)(this.explode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_restart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtFlag;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.PictureBox explode;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweep
+{
+    //0: not assigned
+    //1-8: numbers of mines around the cell
+    //10: mines
+    //20: empty
+    
+    public partial class Form1 : Form
+    {
+        byte[,] Positions = new byte[15, 15];
+        Button[,] ButtonList = new Button[15, 15];
+        System.Media.SoundPlayer soundPlayer = new System.Media.SoundPlayer("C:\\Users\\kient\\source\\repos\\Minesweep\\Resources\\vine_boom.wav");
+        System.Media.SoundPlayer win = new System.Media.SoundPlayer("C:\\Users\\kient\\source\\repos\\Minesweep\\Resources\\Tada.wav");
+        public Form1()
+        {
+            InitializeComponent();
+            //this.Controls.Add(explode);
+            GenerateMines();
+            GeneratePositionValue();
+            GenerateButton();
+        }
+
+        Random rnd = new Random();
+        private void GenerateMines()
+        {
+            int mines = 30;
+            while(mines != 0)
+            {
+                int x = rnd.Next(1, 15);
+                int y = rnd.Next(1, 15);
+                if (Positions[x,y] == 0)
+                {
+                    Positions[x,y] = 10;
+                    mines--;
+                }
+            }
+        }
+
+        private void GeneratePositionValue()
+        {
+            for(int x = 0; x < 15; x++)
+            {
+                for(int y = 0; y < 15; y++)
+                {
+                    byte minesCounter = 0; //number of adjacent mines
+                    //avoid checking mine cell
+                    if (Positions[x, y] == 10) continue;
+                    
+                    for (int countX = -1; countX < 2; countX++)
+                    {
+                        int checkX = x + countX;
+
+                        for (int countY = -1; countY < 2; countY++)
+                        {
+                            int checkY = y + countY;
+
+                            //cell out of bound, avoid checking these cells
+                            if (checkX == -1 || checkY == -1 || checkX > 14 || checkY > 14) continue;
+
+                            if (checkX == x && checkY == y) continue;
+
+                            if (Positions[checkX, checkY] == 10) minesCounter++;
+                        }
+                    }
+
+                    if (minesCounter == 0)
+                        Positions[x, y] = 20;
+                    else
+                        Positions[x, y] = minesCounter;
+                }
+            }
+        }
+
+
+        private void GenerateButton()
+        {
+            int xLocation = 7;
+            int yLocation = 10;
+            for(int x = 0; x < 15; x++)
+            {
+                for(int y = 0; y < 15; y++)
+                {
+                    Button btn = new Button();
+                    btn.Parent = panel1;
+                    btn.Location = new Point(xLocation, yLocation);
+                    btn.Size = new Size(26, 26);
+                    btn.Tag = $"{x},{y}";
+                    btn.Click += BtnClick;
+                    btn.MouseUp += Btn_MouseUp;
+                    xLocation += 26;
+                    ButtonList[x,y] = btn;
+                }
+                yLocation += 26;
+                xLocation = 7;
+            }
+        }
+        private void BtnClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int x = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(0));
+            int y = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(1));
+            byte value = Positions[x, y];
+
+            if(value == 10)
+            {
+                soundPlayer.Play();
+                btn.Image = Properties.Resources.mines;
+
+                explode.Location = new Point(
+                    btn.Location.X - (btn.Width - explode.Width) /6,
+                    btn.Location.Y - (btn.Height - explode.Height) 
+                );
+
+                explode.BackColor = Color.Transparent;
+
+                explode.Visible = true;
+                panel1.Enabled = false;
+                Timer timer = new Timer();
+                timer.Interval = 800; // Thời gian hiển thị GIF (ms)
+                timer.Tick += (s, args) =>
+                {
+                    explode.Visible = false;
+                    timer.Stop();
+                };
+                timer.Start();
+                MessageBox.Show("Game Over");
+                
+            }
+            else if(value == 20)
+            {
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderSize = 1;
+                btn.FlatAppearance.BorderColor = SystemColors.ControlDark;
+                btn.Enabled = false;
+                OpenAdjacentCell(btn);
+                points++;
+            }
+            else
+            {
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderColor = SystemColors.ControlDark;
+                btn.FlatAppearance.MouseDownBackColor = btn.BackColor;
+                btn.FlatAppearance.MouseOverBackColor = btn.BackColor;
+                btn.Text = Positions[x,y].ToString();
+                btn.Font = new Font(btn.Font.Name, btn.Font.Size, FontStyle.Bold);
+                switch (Positions[x, y])
+                {
+                    case 1:
+                        btn.ForeColor = Color.Blue;
+                        break;
+                    case 2:
+                        btn.ForeColor = Color.Green;
+                        break;
+                    case 3:
+                        btn.ForeColor= Color.Red;
+                        break;
+                    case 4:
+                        btn.ForeColor = Color.DarkBlue;
+                        break;
+                    case 5:
+                        btn.ForeColor = Color.DarkRed;
+                        break;
+                    case 6:
+                        btn.ForeColor = Color.DarkCyan;
+                        break;
+                    case 7:
+                        btn.ForeColor = Color.Purple;
+                        break;
+                    case 8:
+                        btn.ForeColor = Color.LightGray;
+                        break;
+                }
+                points++;
+            }
+            btn.Click -= BtnClick;
+            txtScore.Text = points.ToString();
+
+            int totalSafeCells = 15 * 15 - 30; // Tổng số ô không phải mìn
+            if (points == totalSafeCells)
+            {
+                //panel1.Enabled = false;
+                win.Play();
+                MessageBox.Show("Congratulations! You win!");
+                btn_restart_Click(btn_restart, EventArgs.Empty);
+            }
+        }
+
+        private void OpenAdjacentCell(Button btn)
+        {
+            int x = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(0));
+            int y = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(1));
+
+            List<Button> emptyButtons = new List<Button>();
+
+            for (int countX = -1; countX < 2; countX++)
+            {
+                int checkX = x + countX;
+
+                for (int countY = -1; countY < 2; countY++)
+                {
+                    int checkY = y + countY;
+
+                    //cell out of bound, avoid checking these cells
+                    if (checkX < 0 || checkY < 0 || checkX > 14 || checkY > 14) continue;
+
+                    if (checkX == x && checkY == y) continue;
+
+                    Button btnAdjacent = ButtonList[checkX, checkY];
+
+                    if (btnAdjacent == null) continue;
+
+                    if (btnAdjacent.Tag == null || !btnAdjacent.Tag.ToString().Contains(","))
+                    {
+                        continue;
+                    }
+
+                    int xAdjacent = Convert.ToInt32(btnAdjacent.Tag.ToString().Split(',').GetValue(0));
+                    int yAdjacnet = Convert.ToInt32(btnAdjacent.Tag.ToString().Split(',').GetValue(1));
+
+                    byte value = Positions[xAdjacent, yAdjacnet];
+
+                    if (value ==20)
+                    {
+                        if(btnAdjacent.FlatStyle != FlatStyle.Flat)
+                        {
+                            btnAdjacent.FlatStyle = FlatStyle.Flat;
+                            btnAdjacent.FlatAppearance.BorderSize = 1;
+                            btnAdjacent.FlatAppearance.BorderColor = SystemColors.ControlDark;
+                            btnAdjacent.Enabled = false;
+                            emptyButtons.Add(btnAdjacent);
+                            points++;
+                        }
+                    }
+                    else if(value != 10)
+                    {
+                        btnAdjacent.PerformClick();
+                    }
+                }
+            }
+
+            foreach(var btnEmpty in emptyButtons)
+            {
+                OpenAdjacentCell(btnEmpty);
+            }
+
+            txtScore.Text = points.ToString();
+
+        }
+
+        int flag = 30;
+
+        int points = 0;
+
+        private void Btn_MouseUp(object sender, MouseEventArgs e)
+        {
+            txtFlag.Text = "30";
+            if (e.Button == MouseButtons.Right)
+            {
+                Button btn = (Button)sender;
+                if (btn.FlatStyle != FlatStyle.Flat)
+                {
+                    if (btn.Image == null)
+                    {
+                        btn.Image = Properties.Resources.flag;
+                        flag--;
+                        btn.Click -= BtnClick;
+                    }
+                    else
+                    {
+                        btn.Image = null;
+                        flag++;
+                        btn.Click += BtnClick;
+                    }
+                    txtFlag.Text = flag.ToString();
+                }
+            }
+        }
+
+        private void btn_restart_Click(object sender, EventArgs e)
+        {
+            points = 0;
+            flag = 30;
+            txtFlag.Text = flag.ToString();
+            for (int x = 0; x < 15; x++)
+            {
+                for(int y = 0; y < 15; y++)
+                {
+                    ButtonList[x,y].Dispose();
+                }
+            }
+            txtScore.Text = "0";
+            txtFlag.Text = "30";
+            panel1.Controls.Clear();
+            panel1.Enabled = true;
+            ButtonList = new Button[15, 15];
+            Positions = new byte[15, 15];
+            GenerateMines();
+            GeneratePositionValue();
+            GenerateButton();
+        }
+    }
+}
